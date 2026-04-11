6fadfd58264efffcc7b8ddb3b4ad16edf649dcef
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-11 19:56:16 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "Casino Backend Running" });
+});
+
+app.listen(5000, () =>
+  console.log("🚀 Backend running at http://localhost:5000")
+);

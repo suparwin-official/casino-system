3fe6497cb9b02430f68979f38825d25e625aee69
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-11 20:18:06 +0600

    Delete game-engine directory

--- game-engine/slot.logic.js
+++
@@ -1,8 +0,0 @@
-export function spinSlot(bet) {
-  const rtp = 0.95;
-  const roll = Math.random();
-
-  if (roll < 0.05 * rtp) return bet * 10;
-  if (roll < 0.25 * rtp) return bet * 2;
-  return 0;
-}

5246e8b53a0ab3ca3508de02963f4dc9425ebacb
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-11 19:59:13 +0600

    Update wallet.routes.js

--- backend/wallet/wallet.routes.js
+++ backend/wallet/wallet.routes.js
@@ -1,1 +1,8 @@
+import mongoose from "mongoose";
 
+const walletSchema = new mongoose.Schema({
+  userId: String,
+  balance: { type: Number, default: 1000 }
+});
+
+export default mongoose.model("Wallet", walletSchema);

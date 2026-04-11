b785f28184bb06fbc2596c58fed8b1d1d1327935
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-11 20:21:15 +0600

    Delete backend directory

--- backend/wallet/wallet.routes.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const walletSchema = new mongoose.Schema({
-  userId: String,
-  balance: { type: Number, default: 1000 }
-});
-
-export default mongoose.model("Wallet", walletSchema);

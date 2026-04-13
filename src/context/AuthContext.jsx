18b03be080785a1cc7d5bbf10afaec4433d2055c
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-13 23:25:48 +0600

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,28 @@
+import { createContext, useContext, useState } from "react";
 
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+
+  const login = (email) => {
+    const fakeUser = { email, balance: 1000 };
+    localStorage.setItem("user", JSON.stringify(fakeUser));
+    setUser(fakeUser);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);

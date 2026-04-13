5bf02b88267b238aef2d6d84498c68fd49ee7da2
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-13 23:25:09 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Casino from "./pages/Casino";
+import Navbar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/casino"
+          element={
+            <ProtectedRoute>
+              <Casino />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+}

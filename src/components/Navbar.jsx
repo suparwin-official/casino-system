be59b86ce12771b5489ce74fc696e27b9de94125
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-13 23:26:49 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
+export default function Navbar() {
+  const { user, logout } = useAuth();
+
+  return (
+    <nav className="nav">
+      <h2>🎰 Casino</h2>
+      <div>
+        {user && <span>৳{user.balance}</span>}
+        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Link to="/login">Login</Link>}
+      </div>
+    </nav>
+  );
+}
